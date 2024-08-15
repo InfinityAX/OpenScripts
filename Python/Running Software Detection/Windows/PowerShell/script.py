8b9c0f236b2624cf8236506cab759f3c1da2c8f0
Alain Xu <agent.xenial@protonmail.com>
2024-08-14 20:05:26 -0400

    OpenScripts Update 2
    [Python\Running Software Detection\Windows\WMIC]:
    
    Fixed a bug where executables with spaces in their name are not detected as running when running. The bug is caused by the string split() method, which treats spaces as list separators. This has been fixed by replacing split() with splitlines() and additional condition statements to remove empty strings and additional whitespaces from existing strings.
    
    Minor adjustments to the comments.
    
    [Python\Running Software Detection\Windows\PowerShell]:
    First stable release.

--- Python/Running Software Detection/Windows/PowerShell/script.py
+++ Python/Running Software Detection/Windows/PowerShell/script.py
@@ -1,76 +1,99 @@
-# !!! WORK IN PROGRESS !!!
-
-# A Python script that can check if a given executable is running. Also works with lists.
-# Credits: Alain Xu (https://github.com/InfinityAX), Traditional_Candy923 (https://www.reddit.com/user/Traditional_Candy923)
+# A Python script that can check if a given process name is running. Also works with lists.
+# Credits: Alain Xu (https://github.com/InfinityAX), Traditional_Candy923 (https://www.reddit.com/user/Traditional_Candy923), SimonS (https://superuser.com/users/458113/simons), Alex_P (https://stackoverflow.com/users/10294689/alex-p)
 # Version: 0.0
 # NOTES:
-#   - This script utilizes the WMI PowerShell Cmdlets through the command prompt.
+#   - This script utilizes the WMI PowerShell Cmdlets through the command prompt. This will only work if you have a Windows version with PowerShell.
 #   - This version is only for Windows and has only been tested on Windows 10 22H2 Home Edition 64-bits.
-#   - The following functions do not address to potential issue where multiple software may have the same executable name.
-#   - The first few indexes in the output list for get_running_program_list() are going to contain "junk" data.
-#   - Consider adding checks for empty or known "bad" inputs.
+#   - The following functions do not address to potential issue where multiple programs may have the same process name.
+#   - Consider adding checks for empty or known "bad" and duplicated inputs.
 
 
 # Imports the os module needed to open, execute, and read the Windows command prompt
 import os
 
 
-# "os.popen('wmic process get description').read().split()" gets a list of currently running program
-# Returns list of running program
-print(os.popen('wmic process get description').read().split())
+# "os.popen('powershell -command "(Get-Process *).Name"').read().splitlines()" gets a list of currently running processes by running on of the WMI PowerShell Cmdlets commands in PowerShell through the command line
+print(os.popen('powershell -command "(Get-Process *).Name"').read().splitlines())
 
 
-# Checks if the user defined program is running
-# Returns true if the program is running
+# Checks if the user defined process name is running
+# Returns true if a match is found
 # Returns false otherwise
-def is_program_running(programExecutable: str)->bool:
+def is_this_process_running(processName: str)->bool:
     # Checks for matching string in the list and returns the appropriate output
-    if (programExecutable in get_running_program_list()):
+    if (processName in os.popen('powershell -command "(Get-Process *).Name"').read().splitlines()):
         return True
     else:
         return False
 
 
-# Checks if the program are running from a user defined list
-# Returns list of user defined program that are running
-def are_program_running (programExecutableList: list)->list:
-    returnExecutableList = []
+# Checks if the process names from a user defined list are running
+# Returns list of user defined process names that have been found
+def which_of_these_processes_are_running(processNameList: list)->list:
+    # "runningProcessList" is a temporary varible made to store the list of running executes to prevent repeated calls to
+    # the resource intensive "os.popen('powershell -command "(Get-Process *).Name"').read().splitlines()" 
+    runningProcessList = os.popen('powershell -command "(Get-Process *).Name"').read().splitlines()
+    returnProcessNameList = []
     
-    # The following nested for loops and if statements checks if each program in the user defined list
-    # is running. If the program is running and has not been added to the return list, it gets added to
-    # the return list
-    for userProgram in programExecutableList:
-        for runningProgram in os.popen('wmic process get description').read().split():
-            if (userProgram == runningProgram):
-                if (userProgram not in returnExecutableList):
-                    returnExecutableList.append(userProgram)
+    # The following nested for loop and if statement checks if each process name in the user defined list is running.
+    # If a match is found, it gets added to the return list
+    for processName in processNameList:
+        if (processName in runningProcessList):
+            returnProcessNameList.append(processName)
     
-    return returnExecutableList
+    return returnProcessNameList
 
 
-# Checks if all of the program are running from a user defined list
-# Returns true if all of the user defined program are running
+# Checks if all of the process names from a user defined list are running
+# Returns true if all of the user defined process names are found
 # Returns false otherwise
-def are_all_program_running (programExecutableList: list)->bool:
-    # Checks if the user defined program list matches the return list from are_program_running() function
-    if (programExecutableList == are_program_running(programExecutableList)):
+def are_all_these_processes_running (processNameList: list)->bool:
+    runningProcessList = os.popen('powershell -command "(Get-Process *).Name"').read().splitlines()
+    processNameMatchList = []
+    
+    for processName in processNameList:
+        if (processName in runningProcessList):
+            processNameMatchList.append(processName)
+
+    # The above code is mostly the same as the one used in "which_of_these_processes_are_running()"
+    # Below is the additional check to see if the found process name list matches the user defined list
+    if (processNameList == processNameMatchList):
         return True
     else:
         return False
+
+
+# Checks if at least one of the process names from a user defined list are running
+# Returns True if a match is found
+# Returns false otherwise
+def is_at_least_one_of_these_processes_running(processNameList: list)->bool:
+    # The code block is mostly the same as "which_of_these_processes_are_running()", but with "returnProcessNameList" removed,
+    # an immediate return True upon a match and a return False at the end
+    runningProcessList = os.popen('powershell -command "(Get-Process *).Name"').read().splitlines()
+
+    for processName in processNameList:
+        if (processName in runningProcessList):
+            return True
+    
+    return False
 
 
 # Test code for debugging purposes
-print(get_running_program_list())
-print(is_program_running(""))
-print(is_program_running("r447fhgnjkrdg34247"))
-print(is_program_running("obs64.exe"))                          # obs64.exe is OBS Studios for 64-bit Windows
-print(are_program_running([]))
-print(are_program_running([None, 54, []]))
-print(are_program_running(["grdgdrg", "5645645765"]))
-print(are_program_running(["obs64.exe"]))
-print(are_program_running(["msedge.exe", "Code.exe"]))          # msedge.exe: Microsoft Edge | Code.exe: Visual Studio Code
-print(are_all_program_running([]))
-print(are_all_program_running([None, 54, []]))
-print(are_all_program_running(["grdgdrg", "5645645765"]))
-print(are_all_program_running(["obs64.exe"]))
-print(are_all_program_running(["msedge.exe", "Code.exe"]))
+print(is_this_process_running(""))
+print(is_this_process_running("r447fhgnjkrdg34247"))
+print(is_this_process_running("obs64"))                          # obs64 is OBS Studios for 64-bit Windows
+print(which_of_these_processes_are_running([]))
+print(which_of_these_processes_are_running([None, 54, []]))
+print(which_of_these_processes_are_running(["grdgdrg", "5645645765"]))
+print(which_of_these_processes_are_running(["obs64"]))
+print(which_of_these_processes_are_running(["msedge", "Unity Hub"]))          # msedge: Microsoft Edge | Unity Hub: Unity Hub
+print(are_all_these_processes_running([]))
+print(are_all_these_processes_running([None, 54, []]))
+print(are_all_these_processes_running(["grdgdrg", "5645645765"]))
+print(are_all_these_processes_running(["obs64"]))
+print(are_all_these_processes_running(["msedge", "Unity Hub"]))
+print(is_at_least_one_of_these_processes_running([]))
+print(is_at_least_one_of_these_processes_running([None, 54, []]))
+print(is_at_least_one_of_these_processes_running(["grdgdrg", "5645645765"]))
+print(is_at_least_one_of_these_processes_running(["obs64"]))
+print(is_at_least_one_of_these_processes_running(["msedge", "Unity Hub"]))
--- Python/Running Software Detection/Windows/WMIC/script.py
+++ Python/Running Software Detection/Windows/WMIC/script.py
@@ -1,11 +1,11 @@
-# A Python script that can check if a given executable is running. Also works with lists.
-# Credits: Alain Xu (https://github.com/InfinityAX), Traditional_Candy923 (https://www.reddit.com/user/Traditional_Candy923)
-# Version: 1.0
+# A Python script that can check if a given executable name is running. Also works with lists.
+# Credits: Alain Xu (https://github.com/InfinityAX), Traditional_Candy923 (https://www.reddit.com/user/Traditional_Candy923), Mark Rushakoff (https://stackoverflow.com/users/126042/mark-rushakoff), Mahmudul Hasan (https://stackoverflow.com/users/5571663/mahmudul-hasan)
+# Version: 2.0
 # NOTES:
 #   - This script utilizes Microsoft's WMIC interface for WMI which has be deprecated in newer versions of Windows.
 #   - This version is only for Windows and has only been tested on Windows 10 22H2 Home Edition 64-bits.
 #   - The following functions do not address to potential issue where multiple executables may have the same name.
-#   - The first few indexes in the output list for "os.popen('wmic process get description').read().split()" are going to contain "junk" data.
+#   - The first few indexes in the output list for "os.popen('wmic process get description').read().splitlines()" are going to contain "junk" data.
 #   - Consider adding checks for empty or known "bad" and duplicated inputs.
 
 
@@ -13,16 +13,26 @@
 import os
 
 
-# "os.popen('wmic process get description').read().split()" creates a list of executables that are currently running.
-print(os.popen('wmic process get description').read().split())
+# "os.popen('wmic process get description').read().splitlines()" creates a list of executables that are currently running using the WMIC interface for WMI through the command line
+runningExecutableList = os.popen('wmic process get Name').read().splitlines()
+# Due to how WMIC presents the result, some extra string processing needs to be done to remove empty strings and whitespaces
+for listIndex in range(runningExecutableList.__len__()):
+    runningExecutableList[listIndex] = runningExecutableList[listIndex].strip()
+runningExecutableList = list(filter(("").__ne__, runningExecutableList))
+print(runningExecutableList)
 
 
 # Checks if the user defined executable name matches with any executables that are running
 # Returns true if a match is found
 # Returns false otherwise
 def is_this_executable_running(executableName: str)->bool:
+    runningExecutableList = os.popen('wmic process get Name').read().splitlines()
+    # Due to how WMIC presents the result, some extra string processing needs to be done to remove empty strings and whitespaces
+    for listIndex in range(runningExecutableList.__len__()):
+        runningExecutableList[listIndex] = runningExecutableList[listIndex].strip()
+    runningExecutableList = list(filter(("").__ne__, runningExecutableList))
     # Checks for matching string in the list and returns the appropriate output
-    if (executableName in os.popen('wmic process get description').read().split()):
+    if (executableName in runningExecutableList):
         return True
     else:
         return False
@@ -31,12 +41,16 @@ def is_this_executable_running(executableName: str)->bool:
 # Checks if the executable names from a user defined list matches any of the ones running
 # Returns list of executable names from the user defined list that are found running
 def which_of_these_executables_are_running(executableNameList: list)->list:
-    # "runningExecutableList" is a temporary varible made to stare the list of running executes to prevent repeated calls to
-    # the resource intensive "os.popen('wmic process get description').read().split()" 
-    runningExecutableList = os.popen('wmic process get description').read().split()
+    # "runningExecutableList" is a temporary varible made to store the list of running executes to prevent repeated calls to
+    # the resource intensive "os.popen('wmic process get description').read().splitlines()" 
+    runningExecutableList = os.popen('wmic process get Name').read().splitlines()
+    # Due to how WMIC presents the result, some extra string processing needs to be done to remove empty strings and whitespaces
+    for listIndex in range(runningExecutableList.__len__()):
+        runningExecutableList[listIndex] = runningExecutableList[listIndex].strip()
+    runningExecutableList = list(filter(("").__ne__, runningExecutableList))
     returnExecutableNameList = []
     
-    # The following nested for loops and if statements checks if each executable name in the user defined list is running.
+    # The following nested for loop and if statement checks if each executable name in the user defined list is running.
     # If a match is found, it gets added to the return list
     for executableName in executableNameList:
         if (executableName in runningExecutableList):
@@ -49,7 +63,11 @@ def which_of_these_executables_are_running(executableNameList: list)->list:
 # Returns true if all the user defined executable names are found
 # Returns false otherwise
 def are_all_these_executables_running(executableNameList: list)->bool:
-    runningExecutableList = os.popen('wmic process get description').read().split()
+    runningExecutableList = os.popen('wmic process get Name').read().splitlines()
+    # Due to how WMIC presents the result, some extra string processing needs to be done to remove empty strings and whitespaces
+    for listIndex in range(runningExecutableList.__len__()):
+        runningExecutableList[listIndex] = runningExecutableList[listIndex].strip()
+    runningExecutableList = list(filter(("").__ne__, runningExecutableList))
     executableNameMatchList = []
     
     for executableName in executableNameList:
@@ -70,7 +88,11 @@ def are_all_these_executables_running(executableNameList: list)->bool:
 def is_at_least_one_of_these_executables_running(executableNameList: list)->bool:
     # The code block is mostly the same as "which_of_these_executables_are_running()", but with "returnExecutableNameList" removed,
     # an immediate return True upon a match and a return False at the end
-    runningExecutableList = os.popen('wmic process get description').read().split()
+    runningExecutableList = os.popen('wmic process get Name').read().splitlines()
+    # Due to how WMIC presents the result, some extra string processing needs to be done to remove empty strings and whitespaces
+    for listIndex in range(runningExecutableList.__len__()):
+        runningExecutableList[listIndex] = runningExecutableList[listIndex].strip()
+    runningExecutableList = list(filter(("").__ne__, runningExecutableList))
 
     for executableName in executableNameList:
         if (executableName in runningExecutableList):
@@ -87,14 +109,14 @@ print(which_of_these_executables_are_running([]))
 print(which_of_these_executables_are_running([None, 54, []]))
 print(which_of_these_executables_are_running(["grdgdrg", "5645645765"]))
 print(which_of_these_executables_are_running(["obs64.exe"]))
-print(which_of_these_executables_are_running(["msedge.exe", "Code.exe"]))          # msedge.exe: Microsoft Edge | Code.exe: Visual Studio Code
+print(which_of_these_executables_are_running(["msedge.exe", "Unity Hub.exe"]))          # msedge.exe: Microsoft Edge | Unity Hub.exe: Unity Hub
 print(are_all_these_executables_running([]))
 print(are_all_these_executables_running([None, 54, []]))
 print(are_all_these_executables_running(["grdgdrg", "5645645765"]))
 print(are_all_these_executables_running(["obs64.exe"]))
-print(are_all_these_executables_running(["msedge.exe", "Code.exe"]))
+print(are_all_these_executables_running(["msedge.exe", "Unity Hub.exe"]))
 print(is_at_least_one_of_these_executables_running([]))
 print(is_at_least_one_of_these_executables_running([None, 54, []]))
 print(is_at_least_one_of_these_executables_running(["grdgdrg", "5645645765"]))
 print(is_at_least_one_of_these_executables_running(["obs64.exe"]))
-print(is_at_least_one_of_these_executables_running(["msedge.exe", "Code.exe"]))
+print(is_at_least_one_of_these_executables_running(["msedge.exe", "Unity Hub.exe"]))
